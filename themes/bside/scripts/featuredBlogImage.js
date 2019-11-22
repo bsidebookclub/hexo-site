3e71e057d0181e996cb227dd1fdc96e57f7cbbce
Robert Pirtle <astropirtle@gmail.com>
2019-11-22 13:44:02 -0800

    add featured photos to blog posts

---
+++ themes/bside/scripts/featuredBlogImage.js
@@ -0,0 +1,16 @@
+hexo.extend.helper.register('featuredImage', function(post){
+  if(post && post._content) {
+    let content = post._content;
+    const imgReg = /!\[(.*?)\]\((.*?)\)/g;
+    let matches = content.match(imgReg);
+
+    let theMatch = '/img/bside-logo.svg';
+    if(matches && matches.length) {
+      theMatch = matches[0].match(/\((.*?)\)/g)[0];
+      theMatch = theMatch.substr(1, theMatch.length - 2);
+    }
+    let thumbnail = `<img class='post-featured-image' src='${theMatch}' title='${post.title}' />`;
+
+    return thumbnail;
+  }
+});
