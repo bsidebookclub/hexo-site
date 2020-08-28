5ac12cb932117be8480a885a7ec7ba90567eb9c5
Robert Pirtle <astropirtle@gmail.com>
2020-08-28 11:22:09 -0700

    minify (and uglify) js

--- themes/bside/scripts/postRender.js
+++ themes/bside/scripts/postRender.js
@@ -1,7 +1,8 @@
-const { minify } = require('html-minifier');
+const { minify: minifyHtml } = require('html-minifier');
+const { minify: minifyJS } = require('uglify-js');
 
-hexo.extend.filter.register('after_render:html', function(str, data){
-  return minify(str, {
+hexo.extend.filter.register('after_render:html', function(str){
+  return minifyHtml(str, {
     collapseBooleanAttributes: true,
     collapseInlineTagWhitespace: true,
     collapseWhitespace: true,
@@ -9,4 +10,9 @@ hexo.extend.filter.register('after_render:html', function(str, data){
     minifyCSS: true,
     removeComments: true,
   });
+});
+
+hexo.extend.filter.register('after_render:js', function(str){
+  const minified = minifyJS(str);
+  return minified.code;
 });
